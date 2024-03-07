fbcec69189746aee6d31723af78a4e6b7ac7b462
GauthamBhe <161290308+GauthamBhe@users.noreply.github.com>
2024-03-07 16:04:09 +0530

    updated order page as per gautham recommendation

--- src/samplePackage/Order.java
+++ src/samplePackage/Order.java
@@ -5,5 +5,11 @@ import org.openqa.selenium.By;
 public class Order {
 	
 	private By order = By.xpath("order");
+	private By price = By.xpath("price");
+	
+	public void getPrice() {
+		System.out.println("price is: ");
+	}
+	
 
 }
